f37732c4bdd9fe848d0e4afde6c80c3b732aca8f
glopez <gerardoher6205@gmail.com>
2024-08-06 15:01:17 -0600

    Solucion de errores proyectos vs trabajos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  portfolio_back: 'http://18.225.117.70:1000/api/v1/',
+  app_name: 'Portafolio',
+  user_name: 'Gerardo López Hernández',
 };
